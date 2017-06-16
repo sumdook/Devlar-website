d399c88c6a769ce7e64438f35fce2368d78e8e9d
Soumik Sur <soumiksur.me@gmail.com>
2017-06-16 15:19:17 +0530

    added more interactions

--- js/script.js
+++ js/script.js
@@ -58,16 +58,24 @@ window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
 // SLIDE MOTION 
 function nextItem() {
+  if(currentSlideNumber==6){
+    var $slide = $(".background").eq(currentSlideNumber);
+    console.log("thanks");
+    $slide.removeClass("down-scrolltwo").addClass("last-down-scroll ");
+    $('.dot-container').css('top','27vh');
+    $('.scroll-inst').css('top','28vh');
+  }
+  else{
   var $nextSlide = $(".background").eq(currentSlideNumber);
   var $next2Slide = $(".background").eq(currentSlideNumber+1);
   $nextSlide.removeClass("down-scrolltwo").removeClass("up-scroll").addClass("down-scroll");
-  $next2Slide.removeClass("up-scrolltwo").addClass("down-scrolltwo");
+  $next2Slide.removeClass("down-scrolltwo").addClass("down-scrolltwo");
 
   var $currentDot= $(".dot").eq(currentSlideNumber);
   var $prevDot = $(".dot").eq(currentSlideNumber-1);
   $prevDot.removeClass("current");
   $currentDot.addClass("current");
-
+  }
 
   console.log(currentSlideNumber);
 }
@@ -75,10 +83,22 @@ function nextItem() {
 function previousItem() {
   var $currentSlide = $(".background").eq(currentSlideNumber+1);
   var $nextSlide = $(".background").eq(currentSlideNumber+2);
-  $currentSlide.removeClass("down-scroll").removeClass("up-scrolltwo").addClass("up-scroll");
+  $currentSlide.removeClass("down-scroll").removeClass("last-down-scroll").removeClass("up-scrolltwo").addClass("up-scroll");
   $nextSlide.removeClass("down-scrolltwo").removeClass("up-scroll").addClass("up-scrolltwo");
   var $currentDot= $(".dot").eq(currentSlideNumber);
   var $nextDot = $(".dot").eq(currentSlideNumber+1);
   $nextDot.removeClass("current");
   $currentDot.addClass("current");
+  $('.dot-container').css('top','87vh');
+  $('.scroll-inst').css('top','88vh');
+}
+
+function reset(){
+  for (var i = $(".background").length - 1; i >= 0; i--) {
+    $(".background").eq(i).removeClass("down-scroll").removeClass("last-down-scroll").removeClass("up-scroll").removeClass("down-scrolltwo").removeClass("up-scrolltwo").addClass("background");
+      $(".dot").eq(i).removeClass().addClass("dot");
+      $(".dot").eq(0).addClass("current");
+      currentSlideNumber = 0; 
+    }
+
 }
