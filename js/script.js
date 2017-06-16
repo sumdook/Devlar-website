da35d6bc73a900ede08e9f1797de02a55d9f73cb
Yash Anand <yashanand1910@gmail.com>
2017-06-16 16:53:51 +0530

    Add Vulneye project section

--- js/script.js
+++ js/script.js
@@ -58,7 +58,7 @@ window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
 // SLIDE MOTION 
 function nextItem() {
-  if(currentSlideNumber==6){
+  if(currentSlideNumber==5){
     var $slide = $(".background").eq(currentSlideNumber);
     console.log("thanks");
     $slide.removeClass("down-scrolltwo").addClass("last-down-scroll ");
